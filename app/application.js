c96434807b0ac0e2695cd92db45319f93a17ba01
Emanoel <vianna.emanoel@gmail.com>
2019-05-26 18:32:25 -0300

    master - continuação do desevolvimento do trabalho

--- app/application.js
+++ app/application.js
@@ -1,6 +1,13 @@
-buildMap();
+const COLORS = {
+    INFECTED: '#ff6f69',
+    FREE: '#88d8b0',
+    UNDEFINED: '#cecece',
+    SUSCEPTIBLE: '#ffcc5c'
+};
 
-function buildMap() {
+buildMap('Amebiasis', '2005'); // TODO:
+
+function buildMap(disease, year) {
     var width = 650,
         height = 600;
 
@@ -18,6 +25,14 @@ function buildMap() {
 
     d3.json('data/california-map.json', function (error, ca) {
         d3.csv('data/infectious-disease-data.csv', function (error, data) {
+
+            var filtered = data.filter(function (d) {
+                if (disease === d.Disease && d.Year === year && d.Sex === "Total")
+                    return d;
+            });
+
+            console.log(filtered);
+
             svg.append('path')
                 .datum(topojson.feature(ca, ca.objects.subunits))
                 .attr('class', 'land')
@@ -26,10 +41,23 @@ function buildMap() {
             /* bind feature data to the map */
             svg.selectAll('.subunit')
                 .data(topojson.feature(ca, ca.objects.subunits).features)
-                .enter().append('path')
+                .enter()
+                .append('path')
                 .attr('class', function (d) {
-                    console.log(d.properties.name);
                     return 'subunit ' + d.properties.name;
+                })
+                .style(".subunit", function (d) {
+                    var color;
+                    var result = filtered.find(function (data) {
+                        if (data.Count > 5) {
+                            color = COLORS.FREE;
+                            console.log('opa1');
+                        } else {
+                            color = COLORS.INFECTED;
+                            console.log('opa2');
+                        }
+                    });
+                    return color;
                 })
                 .attr('d', path)
                 .on('mouseover', function (d) { //tooltip
@@ -56,6 +84,12 @@ function buildMap() {
             var div = d3.select('#map').append('div')
                 .attr('class', 'tooltip')
                 .style('opacity', 0);
+
+            /* legend to the map */
+            // TODO:
+
+            /* slider to the years */
+            // TODO:
         });
     });
 }
