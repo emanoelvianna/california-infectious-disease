19752f51e49d13203ee6e9b2c55f32c3f59bb461
Emanoel <vianna.emanoel@gmail.com>
2019-06-08 17:50:43 -0300

    dev - seleção de ano, seleção de condado

--- app/application.js
+++ app/application.js
@@ -166,35 +166,39 @@
     }
 
     function _timeline() {
-      // Time
       var dataTime = d3.range(0, 17).map(function (d) {
         return new Date(2000 + d, 0, 1);
       });
 
-      var sliderTime = d3
+      var sliderRange = d3
         .sliderBottom()
         .min(d3.min(dataTime))
         .max(d3.max(dataTime))
-        .step(1000 * 60 * 60 * 24 * 365)
         .width(400)
         .tickFormat(d3.timeFormat('%Y'))
         .tickValues(dataTime)
-        .default(new Date(1998, 10, 3))
+        .default([new Date(2000, 0, 1), new Date(2005, 0, 1)])
+        .fill('#2196f3')
         .on('onchange', val => {
-          // d3.select('p#value-time').text(d3.timeFormat('%Y')(val));
+          d3.select('p#value-range').text(val.map(d3.timeFormat('%Y')).join('-'));
         });
 
-      var gTime = d3
-        .select('div#slider-time')
+      var gRange = d3
+        .select('div#slider-range')
         .append('svg')
         .attr('width', 500)
         .attr('height', 100)
         .append('g')
         .attr('transform', 'translate(30,30)');
 
-      gTime.call(sliderTime);
+      gRange.call(sliderRange);
 
-      d3.select('p#value-time').text(d3.timeFormat('%Y')(sliderTime.value()));
+      d3.select('p#value-range').text(
+        sliderRange
+          .value()
+          .map(d3.timeFormat('%Y'))
+          .join('-')
+      );
     }
 
     function _buildDistributionChartAboutSex() {
