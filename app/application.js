935d0e5685d922cb90b5c57812ad6fd312ba8f0c
Emanoel <vianna.emanoel@gmail.com>
2019-06-10 23:28:19 -0300

    dev - modificações na distribuição de cores

--- app/application.js
+++ app/application.js
@@ -177,24 +177,60 @@
     function _updateColorsInMap(filteredDiseases) {
       map.selectAll('.subunit')
         .style('fill', function (d) {
-          for (var i = 0; i < filteredDiseases.length; i++) {
-            if (filteredDiseases[i].County === d.properties.name) {
-              if (filteredDiseases[i].upper < '0.898') {
-                return MAP_COLORS.FREE;
-              } else if (filteredDiseases[i].upper < '2.801') {
-                return MAP_COLORS.SUSCEPTIBLE;
-              } else {
-                return MAP_COLORS.INFECTED;
-              }
-            }
+          if (countySelected === d.properties.name) {
+            return MAP_COLORS.SELECTED;
+          }
 
-            if (countySelected === d.properties.name) {
-              return MAP_COLORS.SELECTED;
+          for (var i = 0; i < filteredDiseases.length; i++) {
+            if (filteredDiseases[i].Sex === 'Total' && filteredDiseases[i].County === d.properties.name) {
+              var percent = ((filteredDiseases[i].Count / filteredDiseases[i].Population) * 100);
+              console.log(percent.toFixed(2) * 50);
+              return _getColour('#ffffff', '#ff0000', percent.toFixed(5) * 10);
             }
           }
         });
     }
 
+    function _getColour(start_color, end_color, percent) {
+      // strip the leading # if it's there
+      start_color = start_color.replace(/^\s*#|\s*$/g, '');
+      end_color = end_color.replace(/^\s*#|\s*$/g, '');
+
+      // convert 3 char codes --> 6, e.g. `E0F` --> `EE00FF`
+      if (start_color.length == 3) {
+        start_color = start_color.replace(/(.)/g, '$1$1');
+      }
+
+      if (end_color.length == 3) {
+        end_color = end_color.replace(/(.)/g, '$1$1');
+      }
+
+      // get colors
+      var start_red = parseInt(start_color.substr(0, 2), 16),
+        start_green = parseInt(start_color.substr(2, 2), 16),
+        start_blue = parseInt(start_color.substr(4, 2), 16);
+
+      var end_red = parseInt(end_color.substr(0, 2), 16),
+        end_green = parseInt(end_color.substr(2, 2), 16),
+        end_blue = parseInt(end_color.substr(4, 2), 16);
+
+      // calculate new color
+      var diff_red = end_red - start_red;
+      var diff_green = end_green - start_green;
+      var diff_blue = end_blue - start_blue;
+
+      diff_red = ((diff_red * percent) + start_red).toString(16).split('.')[0];
+      diff_green = ((diff_green * percent) + start_green).toString(16).split('.')[0];
+      diff_blue = ((diff_blue * percent) + start_blue).toString(16).split('.')[0];
+
+      // ensure 2 digits by color
+      if (diff_red.length == 1) diff_red = '0' + diff_red
+      if (diff_green.length == 1) diff_green = '0' + diff_green
+      if (diff_blue.length == 1) diff_blue = '0' + diff_blue
+
+      console.log('#' + diff_red + diff_green + diff_blue);
+      return '#' + diff_red + diff_green + diff_blue;
+    };
 
     function _timeline() {
       var dataTime = d3.range(0, 16).map(function (d) {
